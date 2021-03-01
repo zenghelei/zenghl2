853a7e96efab893888c20f623e756a75cdb8fa96
zenghelei <61904665+zenghelei@users.noreply.github.com>
2021-03-01 21:41:36 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld23localmaster2!");
+        System.out.println("HelloWorld23localmaster5!");
         System.out.println("123");
     }
 }
